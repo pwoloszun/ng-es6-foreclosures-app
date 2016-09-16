6a2b4201ede579d538ca8b2b23270aefdd0db136
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-09-16 15:19:44 +0200

    initial commit

--- client/app/components/shared/rm-data-table/rm-data-table.component.js
+++ client/app/components/shared/rm-data-table/rm-data-table.component.js
@@ -2,12 +2,6 @@ import template from './rm-data-table.component.html';
 import './rm-data-table.component.styl';
 
 const DataTableComponent = {
-  bindings: {
-    items: "<",
-    selectedItem: "<",
-    headers: "<",
-    itemClick: "&"
-  },
   template
 };
 
